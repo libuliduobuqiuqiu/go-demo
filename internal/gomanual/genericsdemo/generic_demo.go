f89a333edfdf41b9a596b23fc1fa02953c284cd8
libuliduobuqiuqiu <mbb929833@gmail.com>
2025-04-28 16:01:29 +0800

    feat: enhance generics demo with new success item function and add unmarshal example

--- internal/gomanual/genericsdemo/generic_demo.go
+++ internal/gomanual/genericsdemo/generic_demo.go
@@ -25,22 +25,34 @@ func (m *Man[T]) GetName() string {
 	return m.Name
 }
 
-func NewSuccessItem[itemClass any](code int, message string, item *itemClass) {
-	tmpJson := successItemJson[itemClass]{0, message, *item}
+func NewSuccessItem[itemClass any](code int, message string, item itemClass) {
+	tmpJson := successItemJson[itemClass]{0, message, item}
 	tmpStr, _ := json.Marshal(tmpJson)
 
 	fmt.Println(string(tmpStr))
 	return
 }
 
-func marshalMan() {
-	a := Man[string]{"linshukai", 22, "zhangdsan"}
-	NewSuccessItem[Man[string]](200, "success", &a)
+func MarshalMan() {
+	a := Man[*string]{Name: "linshukai", Age: 22}
+	fmt.Println(a.Address, a.Address == nil)
+	NewSuccessItem[Man[*string]](200, "success", a)
 }
 
 func PrintMan() {
 	a := Man[string]{"linshukai", 22, "zhangdsan"}
 	fmt.Println(a.GetName())
+}
+
+func UnMarshalMan() error {
+	a := `{"name": "zhangsan"}`
+	m := Man[string]{}
+
+	if err := json.Unmarshal([]byte(a), &m); err != nil {
+		return err
+	}
+	fmt.Println(m)
+	return nil
 }
 
 // 类型约束接口
---
+++ test/manual/generics_test.go
@@ -0,0 +1,14 @@
+package manual
+
+import (
+	"godemo/internal/gomanual/genericsdemo"
+	"testing"
+)
+
+func TestMarshalMan(t *testing.T) {
+	genericsdemo.MarshalMan()
+}
+
+func TestUnMarshalMan(t *testing.T) {
+	genericsdemo.UnMarshalMan()
+}
